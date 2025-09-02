ae7a35eadcd85fd62007e8dc3341208ac18f698f
Ajay Kumar <ajay.kr931@gmail.com>
2025-09-03 00:22:48 +0530

    Update provider.tf

--- Infra/provider.tf
+++ Infra/provider.tf
@@ -7,8 +7,8 @@ terraform {
   }
   backend "azurerm" {
     use_cli              = true                                    # Can also be set via `ARM_USE_CLI` environment variable.
-    tenant_id            = "d76015c1-91ed-4502-986c-b2fd34d63343"  # Can also be set via `ARM_TENANT_ID` environment variable. Azure CLI will fallback to use the connected tenant ID if not supplied.
-    subscription_id      = "bcf304d8-21b8-4358-9c77-54b5dd26f938"  # Can also be set via `ARM_SUBSCRIPTION_ID` environment variable. Azure CLI will fallback to use the connected subscription ID if not supplied.
+    tenant_id            = "e30abb13-e248-442c-b835-29af747e86b6"  # Can also be set via `ARM_TENANT_ID` environment variable. Azure CLI will fallback to use the connected tenant ID if not supplied.
+    subscription_id      = "ecf1c36f-8eba-469e-841f-a595c299e914"  # Can also be set via `ARM_SUBSCRIPTION_ID` environment variable. Azure CLI will fallback to use the connected subscription ID if not supplied.
     resource_group_name  = "myChildResourceGroup"          # Can be passed via `-backend-config=`"resource_group_name=<resource group name>"` in the `init` command.
     storage_account_name = "mystorageaccount129"                              # Can be passed via `-backend-config=`"storage_account_name=<storage account name>"` in the `init` command.
     container_name       = "tfstate"                               # Can be passed via `-backend-config=`"container_name=<container name>"` in the `init` command.
@@ -19,6 +19,6 @@ terraform {
 provider "azurerm" {
   # Configuration options
   features {}
-  subscription_id = "bcf304d8-21b8-4358-9c77-54b5dd26f938"
+  subscription_id = "ecf1c36f-8eba-469e-841f-a595c299e914"
 }
     
